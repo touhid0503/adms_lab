2e161cc474d45ca0e9612238e6d1daf76d2cefbc
Md. Touhidul Islam <110239106+touhid0503@users.noreply.github.com>
2026-03-29 22:51:47 +0600

    Add worker records to Worker2 table

--- page_36_37.sql
+++ page_36_37.sql
@@ -6,3 +6,13 @@ CREATE TABLE Worker2 (
     DEPARTMENT VARCHAR(50),
     JOINING_DATE DATETIME
 );
+
+INSERT INTO Worker2 VALUES
+(1,'Rana','Hamid',100000,'HR','2024-02-20'),
+(2,'Sanjoy','Saha',80000,'Admin','2024-06-11'),
+(3,'Mahmudul','Hasan',300000,'HR','2024-02-20'),
+(4,'Asad','Zaman',500000,'Admin','2024-02-20'),
+(5,'Sajib','Mia',500000,'Admin','2024-06-11'),
+(6,'Alamgir','Kabir',200000,'Account','2024-06-11'),
+(7,'Foridul','Islam',75000,'Account','2024-01-20'),
+(8,'Keshob','Ray',90000,'Admin','2024-04-11');
