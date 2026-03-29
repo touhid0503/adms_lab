3d1132727a3feaa4e5d8fcdd7d837943f2a73d27
Md. Touhidul Islam <110239106+touhid0503@users.noreply.github.com>
2026-03-29 23:55:58 +0600

    Update page_38_39.sql

--- page_38_39.sql
+++ page_38_39.sql
@@ -12,3 +12,10 @@ CREATE TABLE Department (
     deptName VARCHAR(50),
     location VARCHAR(50)
 );
+
+INSERT INTO Teacher VALUES
+(1, 'Mizanur', 'Rahman', 'CSE', 28, 35000),
+(2, 'Delwar', 'Hossain', 'CSE', 26, 33000),
+(3, 'Shafiul', 'Islam', 'EEE', 24, 30000),
+(4, 'Faisal', 'Imran', 'CSE', 30, 50000),
+(5, 'Ahsan', 'Habib', 'English', 28, 28000);
