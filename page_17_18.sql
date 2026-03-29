30b76c70c39aac52e8e7476da39cb04584a2195a
Md. Touhidul Islam <110239106+touhid0503@users.noreply.github.com>
2026-03-29 20:51:57 +0600

    Add worker records and constraints to the database

--- page_17_18.sql
+++ page_17_18.sql
@@ -13,3 +13,13 @@ Alter Table Worker Alter Column DEPARTMENT VARCHAR(100)
 
 Alter Table Worker Add CONSTRAINT checksalary Check (Salary>100)
 Alter Table Worker Add CONSTRAINT uniquefname Unique (FIRST_NAME)
+
+INSERT INTO Worker VALUES
+(1,'Rana','Hamid',100000,'HR','2024-02-20 09:00:00'),
+(2,'Sanjoy','Saha',80000,'Admin','2024-06-11 09:00:00'),
+(3,'Mahmudul','Hasan',300000,'HR','2024-02-20 09:00:00'),
+(4,'Asad','Zaman',500000,'Admin','2024-02-20 09:00:00'),
+(5,'Sajib','Mia',500000,'Admin','2024-06-11 09:00:00'),
+(6,'Alamgir','Kabir',200000,'Account','2024-06-11 09:00:00'),
+(7,'Foridul','Islam',75000,'Account','2024-01-20 09:00:00'),
+(8,'Keshob','Ray',90000,'Admin','2024-04-11 09:00:00');
